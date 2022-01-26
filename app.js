044f7a6430f349ef06796972b69812794b8267a9
Voglas <v.glasner@gmail.com>
2022-01-26 17:46:19 +0200

    fix

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ app.use('/t', require('./routes/redirect.routes'))
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.use('/static', express.static(path.join(__dirname, 'client', 'build')))
-
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
