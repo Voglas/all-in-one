32f3caa92441a786ff0e476d002d9380537b8c9a
Voglas <v.glasner@gmail.com>
2022-01-26 13:39:39 +0200

    fix bug in app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
 
-if (process.env.Node_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
     app.get('*', (req, res) => {
