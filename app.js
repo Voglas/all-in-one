37f1624e200f680794c24798b4c496529aa0c61d
Voglas <v.glasner@gmail.com>
2022-01-27 11:44:59 +0200

    added /static to app.js

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
+    app.use("/static", express.static('./static/'));
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
