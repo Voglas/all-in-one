ae91c16673263348073ed716bcd31d3131cde52a
Voglas <v.glasner@gmail.com>
2022-01-27 10:52:20 +0200

    BACK added static to app

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.use('/static', express.static(path.join(__dirname, 'client', 'build', 'static')))
-
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
